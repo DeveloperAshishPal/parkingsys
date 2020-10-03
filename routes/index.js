62e9006a0d67da1950dd4bb39338c9e9fd8c2881
ashish pal <developerashishpal@gmail.com>
2020-10-03 14:59:36 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var sale = require('./sale.route');
 
 module.exports = (mount,app) => {    
     // loading routes
-    app.use(`${mount}/sale`, slots(router))
-    app.use(`${mount}/slots`,sale(router))
+    app.use("/api/v1/sale", sale(router))
+    app.use("/api/v1/slots",slots(router))
 
   };
